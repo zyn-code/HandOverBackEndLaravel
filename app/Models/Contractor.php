54ddab3bea9794d11d95fd2ebe41d7ba9e5910d1
ZiadYoutanjy <ziadyoutanji123@gmail.com>
2025-06-23 00:24:27 +0300

    zaz

--- app/Models/Contractor.php
+++ app/Models/Contractor.php
@@ -2,10 +2,28 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Contractor extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'phone_number',
+        'service_categories',
+        'location',
+        'years_of_experience',
+        'description',
+    ];
+
+    protected $casts = [
+        'service_categories' => 'array',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
