8fe84a41f75295f9a0bdcc6913e7e80be763918a
ZiadYoutanjy <ziadyoutanji123@gmail.com>
2025-06-23 20:25:52 +0300

    Ziad

--- database/migrations/2025_06_23_172301_create_building_services_table.php
+++ database/migrations/2025_06_23_172301_create_building_services_table.php
@@ -4,23 +4,18 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+return new class CreateBuildingServicesTable extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    public function up()
     {
         Schema::create('building_services', function (Blueprint $table) {
             $table->id();
+            $table->string('service_name');
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('building_services');
     }
--- database/migrations/2025_06_23_172314_create_vehicle_services_table.php
+++ database/migrations/2025_06_23_172314_create_vehicle_services_table.php
@@ -4,23 +4,18 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+return new class CreateVehicleServicesTable extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    public function up()
     {
         Schema::create('vehicle_services', function (Blueprint $table) {
             $table->id();
+            $table->string('service_name');
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('vehicle_services');
     }
