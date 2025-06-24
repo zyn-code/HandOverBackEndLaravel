60dc5c00a8cac6e42418e1fbb13d4863524ab3f6
ZiadYoutanjy <ziadyoutanji123@gmail.com>
2025-06-24 23:24:30 +0300

    zaz

--- database/migrations/2025_06_24_202258_create_car_categories_table.php
+++ database/migrations/2025_06_24_202258_create_car_categories_table.php
@@ -6,20 +6,15 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('car_categories', function (Blueprint $table) {
             $table->id();
+            $table->string('name')->unique();
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('car_categories');
